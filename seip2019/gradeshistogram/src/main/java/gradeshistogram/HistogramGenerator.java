facceaa7a2bfc03783ef0366c7aa401a24d39e6c
skpanagiotis <panagiotis.skias@gmail.com>
2019-03-16 19:07:27 +0200

    Add Docummentation

--- seip2019/gradeshistogram/src/main/java/gradeshistogram/HistogramGenerator.java
+++ seip2019/gradeshistogram/src/main/java/gradeshistogram/HistogramGenerator.java
@@ -4,14 +4,24 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+/***
+ * 
+ * @author PANAGIOTIS The purpose of this class is to generate a histogram by
+ *         given students' grades
+ * 
+ *
+ */
+
 public class HistogramGenerator {
+	/***
+	 * Receives a .txt file with students' grades and make a 2 dimensions histogram.
+	 * 
+	 * @param args the command line arguments
+	 */
 
 	public static void main(String[] args) {
-		System.out.println("Please insert the file with the grades");
-		Scanner input = new Scanner(System.in);
-		String path = input.next();
 		try {
-			int grades[] = readFile(path);
+			int grades[] = readFile(args[0]);
 			JFreeChartXYLineChartDemo demo = new JFreeChartXYLineChartDemo();
 			demo.generateChart(grades);
 		} catch (FileNotFoundException e) {
@@ -20,7 +30,13 @@ public class HistogramGenerator {
 
 	}
 
-	// The method readFile takes a file with grades and return an array with
+	/***
+	 * The method readFile takes a file with grades and return an array with grades
+	 * 
+	 * @param path the path of the .txt file
+	 * @return grades an integer array
+	 * @throws FileNotFoundException if the file does not exist
+	 */
 	// frequency of the grades
 	public static int[] readFile(String path) throws FileNotFoundException {
 		int[] grades = new int[11];
